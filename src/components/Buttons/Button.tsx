517ba3c62dd70d9a443d2323334ed2c3fb990181
Sajeethan Gulendran <73322508+sajeethan19@users.noreply.github.com>
2024-03-08 01:51:48 +0530

    Ready to Publish

---
+++ src/components/Buttons/Button.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./Button.css"
+
+function Button() {
+  return (
+    <div>
+        <button className='SajeeBtn glass-blue'>SajeeBtn</button>
+    </div>
+  )
+}
+
+export default Button
